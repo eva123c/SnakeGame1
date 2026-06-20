e8a2339ec87ef3ac2bf3d659ff80d7c98410ca16
eva123c <victoriaclipa6@gmail.com>
2026-06-20 20:47:13 +0300

    feat(lab04): use STL containers, string and references instead of raw pointers

--- painter.cpp
+++ painter.cpp
@@ -1,4 +1,15 @@
 #include "painter.hpp"
+#include <iostream>
 
-void Painter::DrawImage(Point topLeft, Point bottomRight, char **image) {/* ... */}
-void Painter::WriteText(Point position, char *text) {/* ... */}
+void Painter::DrawImage(Point topLeft, Point bottomRight, const std::vector<std::string>& image)
+{
+    for (const std::string& line : image)
+    {
+        std::cout << line << std::endl;
+    }
+}
+
+void Painter::WriteText(Point position, const std::string& text)
+{
+    std::cout << text << std::endl;
+}
